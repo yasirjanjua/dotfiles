5f35edd747410a6dffe9a0bd65b27fabb4020de8
yasirjanjua <yasir_janjua@hotmail.com>
2019-02-21 14:57:17 +0500

    empty repo

--- git/git-config.sh
+++
@@ -1,3 +0,0 @@
-# git config --global core.editor "'C:/Program Files/Sublime Text 2/sublime_text.exe' -n -w"
-# git config --global push.default upstream
-# git config --global merge.conflictstyle diff3
